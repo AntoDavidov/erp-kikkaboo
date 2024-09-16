de96181fd37d5fa8bd1e86111eefb1b94c7e762a
CrustCrafters <youremail@example.com>
2024-09-16 11:12:19 +0200

    test

--- src/main/java/nl/fontys/s3/erp/configuration/db/DatabaseDataInitializer.java
+++ src/main/java/nl/fontys/s3/erp/configuration/db/DatabaseDataInitializer.java
@@ -6,6 +6,7 @@ import nl.fontys.s3.erp.persistence.ProductRepository;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+//test
 
 //@Component
 //@AllArgsConstructor
