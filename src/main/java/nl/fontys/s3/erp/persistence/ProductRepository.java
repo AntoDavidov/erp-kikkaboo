1a3fbcd2357564210d966256ac170d475442ecf0
CrustCrafters <youremail@example.com>
2024-09-25 14:46:03 +0200

    Made new branch. About to start working on the Products part of the project.

--- src/main/java/nl/fontys/s3/erp/persistence/ProductRepository.java
+++ src/main/java/nl/fontys/s3/erp/persistence/ProductRepository.java
@@ -2,10 +2,26 @@ package nl.fontys.s3.erp.persistence;
 
 import nl.fontys.s3.erp.persistence.entity.ProductEntity;
 
+import java.util.List;
+
 
 public interface ProductRepository{
 
     ProductEntity save(ProductEntity product);
+
+    ProductEntity findById(long id);
+
+    void deleteById(long id);
+
+    boolean existsById(long id);
+
+    boolean existsBySKU(int sku);
+
+    List<ProductEntity> findAll();
+
+    int count();
+
+
 
 
 }
--- src/main/java/nl/fontys/s3/erp/persistence/impl/FakeProductRepositoryImpl.java
+++ src/main/java/nl/fontys/s3/erp/persistence/impl/FakeProductRepositoryImpl.java
@@ -14,9 +14,7 @@ public class FakeProductRepositoryImpl implements ProductRepository {
     private static long NEXT_ID = 1;
     private final List<ProductEntity> savedProducts;
 
-    public FakeProductRepositoryImpl() {
-        this.savedProducts = new ArrayList<ProductEntity>();
-    }
+    public FakeProductRepositoryImpl() { this.savedProducts = new ArrayList<ProductEntity>(); }
 
     @Override
     public ProductEntity save(ProductEntity product) {
@@ -24,10 +22,51 @@ public class FakeProductRepositoryImpl implements ProductRepository {
             product.setProductId(NEXT_ID++);
             NEXT_ID++;
             this.savedProducts.add(product);
+        } else {
+            ProductEntity existingProduct = this.findById(product.getProductId());
+            if(existingProduct != null) {
+                existingProduct.setName(product.getName());
+                existingProduct.setShortName(product.getShortName());
+                existingProduct.setDescription(product.getDescription());
+                existingProduct.setCostPrice(product.getCostPrice());
+                existingProduct.setRecommendedRetailPrice(product.getRecommendedRetailPrice());
+                existingProduct.setManufacturer(product.getManufacturer());
+                existingProduct.setSku(product.getSku());
+                existingProduct.setWholeSalePrice(product.getWholeSalePrice());
+                existingProduct.setWeight(product.getWeight());
+            }
         }
         return product;
+    }
+    @Override
+    public ProductEntity findById(long productId){
+        return this.savedProducts.stream().filter(product -> product.getProductId() == productId).findFirst().orElse(null);
+    }
+
+    @Override
+    public List<ProductEntity> findAll() {
+        return this.savedProducts;
+    }
+
+    @Override
+    public void deleteById(long productId){
+        this.savedProducts.removeIf(p -> p.getProductId() == productId);
+    }
+
+    @Override
+    public boolean existsById(long productId) {
+        return this.savedProducts.stream().anyMatch(product -> product.getProductId() == productId);
+    }
+
+    @Override
+    public boolean existsBySKU(int productSKU){
+        return this.savedProducts.stream().anyMatch(p -> p.getSku() == productSKU);
 
     }
 
+    @Override
+    public int count() {
+        return this.savedProducts.size();
+    }
 
 }
