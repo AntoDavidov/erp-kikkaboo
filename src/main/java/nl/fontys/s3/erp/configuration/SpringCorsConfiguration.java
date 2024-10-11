fe978ccd552d0e4e948c3ab70efa0b3c045ec493
CrustCrafters <youremail@example.com>
2024-10-11 17:02:30 +0200

    Fixed CORS configurator problem from Front-End

--- src/main/java/nl/fontys/s3/erp/configuration/SpringCorsConfiguration.java
+++ src/main/java/nl/fontys/s3/erp/configuration/SpringCorsConfiguration.java
@@ -14,7 +14,11 @@ public class SpringCorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(allowedOrigins);
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
