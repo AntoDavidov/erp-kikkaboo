edd6c8ce4df8e5800e681999bb20a61ae5f0da84
CrustCrafters <youremail@example.com>
2024-12-03 01:26:57 +0100

    Hopefully now the pipeline is fixed.

--- src/test/java/nl/fontys/s3/erp/ErpApplicationTests.java
+++ src/test/java/nl/fontys/s3/erp/ErpApplicationTests.java
@@ -1,10 +1,15 @@
 package nl.fontys.s3.erp;
 
+import nl.fontys.s3.erp.configuration.security.token.impl.AccessTokenEncoderDecoderImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ErpApplicationTests {
+
+    @MockBean
+    private AccessTokenEncoderDecoderImpl accessTokenEncoderDecoder;
 
     @Test
     void contextLoads() {
