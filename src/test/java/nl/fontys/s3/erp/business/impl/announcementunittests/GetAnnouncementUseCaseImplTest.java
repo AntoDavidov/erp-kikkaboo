0e3c23951c9212273818d6c1b026ad158215c9c5
CrustCrafters <youremail@example.com>
2025-01-08 02:34:47 +0100

    Please..

--- src/test/java/nl/fontys/s3/erp/business/impl/announcementunittests/GetAnnouncementUseCaseImplTest.java
+++ src/test/java/nl/fontys/s3/erp/business/impl/announcementunittests/GetAnnouncementUseCaseImplTest.java
@@ -2,24 +2,13 @@ package nl.fontys.s3.erp.business.impl.announcementunittests;
 
 import nl.fontys.s3.erp.business.exceptions.AnnouncementDoesNotExist;
 import nl.fontys.s3.erp.business.impl.announcementsimpl.GetAnnouncementUseCaseImpl;
-import nl.fontys.s3.erp.business.impl.converters.AnnouncementConverter;
-import nl.fontys.s3.erp.domain.announcements.Announcement;
-import nl.fontys.s3.erp.domain.users.Department;
-import nl.fontys.s3.erp.domain.users.Role;
-import nl.fontys.s3.erp.domain.users.User;
 import nl.fontys.s3.erp.persistence.AnnouncementRepository;
-import nl.fontys.s3.erp.persistence.entity.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
